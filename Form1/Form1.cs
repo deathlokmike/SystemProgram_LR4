103a383e923bd2c83ff01434fb33c0f07834436f
Mikhail Drozdov <deathlokmike@gmail.com>
2022-09-30 22:12:31 +0300

    Add project files.

---
+++ Form1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Form1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("MFCLibrary.dll", CharSet = CharSet.Ansi)] private static extern void SendString(string str, int threadNumber);
+        [DllImport("MFCLibrary.dll")] public static extern int SendInt(int n);
+        [DllImport("MFCLibrary.dll")] public static extern int GetInt();
+        [DllImport("MFCLibrary.dll")] private static extern bool ConnectToServer();
+        [DllImport("MFCLibrary.dll")] private static extern void Disconnect();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void add_child(int num_thread)
+        {
+            if (treeView1.Nodes.Count == 0)
+                treeView1.Nodes.Add("Главный поток");
+            if (treeView1.Nodes.Count == 1)
+            {
+                if (treeView1.Nodes[0].Text == "Нет ответа сервера")
+                {
+                    treeView1.Nodes.Clear();
+                    treeView1.Nodes.Add("Главный поток");
+                }
+                treeView1.Nodes.Add("Все потоки");
+            }
+                
+            if (treeView1.Nodes.Count >= 2)
+            {
+                if (num_thread > treeView1.Nodes[1].Nodes.Count)
+                {
+                    int buf = num_thread - treeView1.Nodes[1].Nodes.Count;
+                    for (int i = 0; i < buf; i++)
+                    {
+                        treeView1.Nodes[1].Nodes.Add("Поток " + (treeView1.Nodes[1].Nodes.Count + 1).ToString());
+                    }
+                }
+                else
+                    treeView1.Nodes[1].Nodes.Clear();
+            }
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            foreach (TreeNode child in e.Node.Nodes)
+                child.Checked = e.Node.Checked;
+        }
+        
+        private List<int> get_current_num() 
+        {
+            List<int> buff = new List<int>();
+            if (treeView1.Nodes.Count > 0)
+            {
+                if (treeView1.Nodes[0].Checked)
+                    buff.Add(-2);
+                if (treeView1.Nodes.Count > 1)
+                {
+                    if (treeView1.Nodes[1].Checked)
+                        buff.Add(-1);
+                    else
+                    {
+                        int j = 0;
+                        foreach (TreeNode child in treeView1.Nodes[1].Nodes)
+                        {
+                            if (child.Checked)
+                                buff.Add(j);
+                            j += 1;
+                        }
+                    }    
+                }
+            }
+            return buff;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ConnectToServer())
+            {
+                int maxThreads = Convert.ToInt32(textBox1.Text);
+                SendInt(0);
+                SendInt(maxThreads);
+                add_child(GetInt());
+                Disconnect();
+            }
+            else
+            {
+                treeView1.Nodes.Clear();
+                treeView1.Nodes.Add("Нет ответа сервера");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (treeView1.Nodes.Count != 1)
+            {
+                ConnectToServer();
+                SendInt(1);
+                add_child(GetInt());
+                Disconnect();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (ConnectToServer())
+            {
+                ConnectToServer();
+                SendInt(20);
+                add_child(GetInt());
+                Disconnect();
+            }
+            else
+            {
+                treeView1.Nodes.Clear();
+                treeView1.Nodes.Add("Нет ответа сервера");
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ConnectToServer();
+            SendInt(2);
+            string text = textBox2.Text;
+            List<int> thread_nums = get_current_num();
+            if (text.Length == 0 || thread_nums.Count == 0) return;
+            foreach (int threadNumber in thread_nums)
+            {
+                SendString(text, threadNumber);
+                Disconnect();
+            }
+        }
+    }
+}
---
+++ Lab1/Lab4.cpp
@@ -0,0 +1,161 @@
+﻿// Lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Lab1.h"
+
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// Единственный объект приложения
+CWinApp theApp;
+vector<HANDLE> vCloseEvent;
+vector<HANDLE> vMessageEvent;
+HANDLE hEventAllSend = CreateEvent(NULL, TRUE, FALSE, NULL);
+HANDLE hMutex = CreateMutex(NULL, FALSE, "mutex");
+string message = "";
+
+extern "C"
+{
+	__declspec(dllimport) inline int __stdcall GetInt();
+	__declspec(dllimport) inline char* __stdcall GetString(int& threadNumber);
+	__declspec(dllimport) inline void __stdcall SendInt(int n);
+}
+
+__declspec(dllimport) inline void _stdcall OpenPipe();
+__declspec(dllimport) inline void __stdcall Connect();
+__declspec(dllimport) inline void __stdcall ServerDisconnect();
+
+void ToFile(int num, string& message)
+{
+	string fname = to_string(num + 1) + ".txt";
+	ofstream fout(fname);
+	fout << message;
+	fout.close();
+}
+
+void MyThread(const int& num)
+{
+	WaitForSingleObject(hMutex, INFINITE);
+	cout << "Поток № " << num + 1 << " создан." << endl;
+	ReleaseMutex(hMutex);
+	HANDLE hEvents[] = { vCloseEvent[num], vMessageEvent[num], hEventAllSend };
+	bool flag = true;
+	while (flag)
+	{
+		switch (WaitForMultipleObjects(3, hEvents, FALSE, INFINITE) - WAIT_OBJECT_0)
+		{
+		case 0:
+		{
+			WaitForSingleObject(hMutex, INFINITE);
+			cout << "Поток № " << num + 1 << " завершен." << endl;
+			ReleaseMutex(hMutex);
+			flag = false;
+			break;
+		}
+		case 1:
+		case 2:
+		{
+			WaitForSingleObject(hMutex, INFINITE);
+			ToFile(num, message);
+			ResetEvent(hEventAllSend);
+			ReleaseMutex(hMutex);
+			break;
+		}
+		}
+	}
+	return;
+}
+
+void start()
+{
+	OpenPipe();
+	int k = 0;
+	while (true)
+	{
+		Connect();
+		int evNum = GetInt();
+		switch (evNum) {
+		case 0:
+		{
+			const int threadsNumber = GetInt();
+			for (int i = 0; i < threadsNumber; i++)
+			{
+				thread t(MyThread, k++);
+				t.detach();
+				vCloseEvent.push_back(CreateEvent(NULL, FALSE, FALSE, NULL));
+				vMessageEvent.push_back(CreateEvent(NULL, FALSE, FALSE, NULL));
+
+			}
+			break;
+		}
+		case 1:
+		{
+			SetEvent(vCloseEvent[--k]);
+			CloseHandle(vCloseEvent[k]);
+			vCloseEvent.pop_back();
+			break;
+		}
+		case 2:
+		{
+			int threadNumber;
+			char* tmp = GetString(threadNumber);
+			message = string(tmp);
+			delete[] tmp;
+			switch (threadNumber)
+			{
+			case -1:
+			{
+				SetEvent(hEventAllSend);
+				break;
+			}
+			case -2:
+			{
+				cout << "Сообщение от главного потока: " << message << "." << endl;
+				break;
+			}
+			default:
+			{
+				SetEvent(vMessageEvent[threadNumber]);
+				break;
+			}
+			}
+		}
+		}
+		SendInt(vCloseEvent.size());
+		ServerDisconnect();
+	}
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int nRetCode = 0;
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+    cout << "Консольное приложение запущено. " << endl;
+    if (hModule != nullptr)
+    {
+        // инициализировать MFC, а также печать и сообщения об ошибках про сбое
+        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+        {
+            wprintf(L"Критическая ошибка: сбой при инициализации MFC\n");
+            nRetCode = 1;
+        }
+        else
+        {
+			start();
+        }
+    }
+    else
+    {
+        wprintf(L"Критическая ошибка: сбой GetModuleHandle\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
